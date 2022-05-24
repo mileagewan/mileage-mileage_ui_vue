b1f1cccb5220f01dcb27a2d4644f76207a7714ce
mileage <792668520@qq.com>
2022-05-24 12:30:49 +0800

    【story开发/问题单号】：新开发 【作者】：wlc 【修改详情】：配置按需引入

---
+++ webpack.dll.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    manifest: ['vue', 'element-ui'],
+  },
+  // 这个是输出 dll 文件
+  output: {
+    path: path.resolve(__dirname, './dll'),
+    filename: '_dll_[name].js',
+    library: '_dll_[name]',
+  },
+  // 这个是输出映射表
+  plugins: [
+    new webpack.DllPlugin({
+      name: '_dll_[name]', // name === output.library
+      path: path.resolve(__dirname, './dll/[name].manifest.json'),
+    })
+  ]
+};
